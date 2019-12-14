234569db625a9a7fe15b3e34cb0ced50e0087f7a
Zikri Khairan <zikrikhairan156@gmail.com>
2019-12-14 19:16:24 +0700

    [Fariz|Zikri] Add test case for handle sort in TransactionList

--- src/Components/TransactionList.test.js
+++ src/Components/TransactionList.test.js
@@ -8,12 +8,13 @@ describe('TransactionList', () => {
   let secondTransaction;
   let thirdTransaction;
   let transactions;
+  let mockedOnSort;
   beforeEach(() => {
     firstTransaction = {
       id: 1,
       walletId: 1,
       type: 'deposit',
-      amount: 7700000,
+      nominal: 7700000,
       description: 'Payslip 2019-11-28',
       createdAt: '2019-11-28T13:26:15.063Z',
       updatedAt: '2019-11-28T13:26:15.063Z'
@@ -22,22 +23,23 @@ describe('TransactionList', () => {
       id: 2,
       walletId: 1,
       type: 'withdraw',
-      amount: 30,
+      nominal: 30,
       description: 'Buy Cheeseburger for lunch',
-      createdAt: '2019-11-28T13:26:15.063Z',
-      updatedAt: '2019-11-28T13:26:15.063Z'
+      createdAt: '2019-11-27T13:26:15.063Z',
+      updatedAt: '2019-11-27T13:26:15.063Z'
     };
     thirdTransaction = {
       id: 3,
       walletId: 1,
       type: 'withdraw',
-      amount: 100,
+      nominal: 100,
       description: 'Dinner at Italian Steak House',
-      createdAt: '2019-11-28T13:26:15.063Z',
-      updatedAt: '2019-11-28T13:26:15.063Z'
+      createdAt: '2019-11-26T13:26:15.063Z',
+      updatedAt: '2019-11-26T13:26:15.063Z'
     };
+    mockedOnSort = jest.fn();
     transactions = [firstTransaction, secondTransaction, thirdTransaction];
-    wrapper = shallow(<TransactionList transactions={transactions} />);
+    wrapper = shallow(<TransactionList transactions={transactions} onSort={mockedOnSort} />);
   });
   describe('#render', () => {
     it('should render table, thead and tbody', () => {
@@ -48,6 +50,15 @@ describe('TransactionList', () => {
 
     it('should render TransactionItem component with firstTransaction', () => {
       expect(wrapper.find('TransactionItem').at(0).props().transaction).toEqual(firstTransaction);
+    });
+
+    it('should call the onSort when the table header of Date is clicked', async () => {
+      wrapper.find('#date-header').simulate('click', 'date');
+      await flushPromises();
+
+      expect(mockedOnSort).toHaveBeenCalled();
+      expect(mockedOnSort).toHaveBeenCalledTimes(1);
+      expect(mockedOnSort).toHaveBeenCalledWith('date', 'asc');
     });
   });
 });
